da46ea62027dcb1d27b885f4f898065ec9c7980c
Eli Williamson <eliwilliamson@gmail.com>
2016-12-28 17:15:01 -0500

    init header partial

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-// JS Goes here - ES6 supported
+// Sticky Nav Functionality
+
+$(window).on('scroll', function(){
+
+  var currentWindowPos = $(window).scrollTop();
+
+  if (currentWindowPos > 0) {
+    $("#header").addClass('scrolled');
+  } else {
+    $("#header").removeClass('scrolled');
+  }
+});
