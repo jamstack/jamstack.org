21e325c9df362446a284e5fb0a5496936f135d99
Eli Williamson <eliwilliamson@gmail.com>
2016-12-29 12:34:07 -0500

    convert jquery code to vanilla javascript for lighter script weight

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,36 @@
-// Sticky Nav Functionality
+// Sticky Nav Functionality in Vanilla JS
 
-$(window).on('scroll', function(){
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
 
-  var currentWindowPos = $(window).scrollTop();
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
+
+var header = document.getElementById("header");
+
+window.onscroll = function() {
+
+  var currentWindowPos = document.documentElement.scrollTop || document.body.scrollTop;
 
   if (currentWindowPos > 0) {
-    $("#header").addClass('scrolled');
+    addClass(header, 'scrolled');
   } else {
-    $("#header").removeClass('scrolled');
+    removeClass(header, 'scrolled');
   }
-});
+};
