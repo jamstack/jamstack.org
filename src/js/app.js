75ef622b2435e41fb55a7f7e45eb914d456c0315
Eli Williamson <eliwilliamson@gmail.com>
2017-03-07 16:32:38 -0500

    add bottom cta (how section) to landing page

--- src/js/app.js
+++ src/js/app.js
@@ -34,3 +34,25 @@ window.onscroll = function() {
     removeClass(header, 'scrolled');
   }
 };
+
+
+// scrolling for anchor links
+
+$(document).ready(function(){
+  $(".scrolling-link").click(function(e) {
+    e.preventDefault();
+
+    anchorScroll( $(this), $($(this).attr("href")), 1000 );
+  });
+});
+
+function anchorScroll(this_obj, that_obj, base_speed) {
+  var this_offset = this_obj.offset();
+  var that_offset = that_obj.offset();
+  var offset_diff = Math.abs(that_offset.top - this_offset.top);
+  var speed       = 1000;
+
+  $("html,body").animate({
+    scrollTop: Math.abs(that_offset.top - 125)
+  }, speed);
+}
