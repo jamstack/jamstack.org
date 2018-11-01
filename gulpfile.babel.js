c6043f8a29bab289c5357f8f76d405e33816b99b
Mattt <mattt@me.com>
2018-11-01 11:44:18 -0700

    Update to Gulp 4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,76 +19,83 @@ const hugoBin = "hugo";
 const defaultArgs = ["-d", "../dist", "-s", "site", "-v"];
 const DEST = "./dist/";
 
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
-
-gulp.task("build", ["css", "js", "fonts", "images", "hugo"]);
-gulp.task("build-preview", ["css", "js", "fonts", "images", "hugo-preview"]);
-
 gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
+    gulp.src("./src/css/*.css")
     .pipe(postcss([
-      cssImport({from: "./src/css/main.css"}),
-      neatgrid(),
-      nestedcss(),
-      colorfunctions(),
-      hdBackgrounds(),
-      cssextend(),
-      cssvars({variables: styleVariables})]))
-    .pipe(gulp.dest(DEST+"css"))
+        cssImport({
+            from: "./src/css/main.css"
+        }),
+        neatgrid(),
+        nestedcss(),
+        colorfunctions(),
+        hdBackgrounds(),
+        cssextend(),
+        cssvars({
+            variables: styleVariables
+        })
+    ]))
+    .pipe(gulp.dest(DEST + "css"))
     .pipe(browserSync.stream())
 ));
 
 gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
+    const myConfig = Object.assign({}, webpackConfig);
 
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
+    webpack(myConfig, (err, stats) => {
+        if (err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            colors: true,
+            progress: true
+        }));
+        browserSync.reload();
+        cb();
+    });
 });
 
 gulp.task("fonts", () => (
-  gulp.src("./src/fonts/**/*")
-    .pipe(gulp.dest(DEST+"fonts"))
+    gulp.src("./src/fonts/**/*")
+    .pipe(gulp.dest(DEST + "fonts"))
     .pipe(browserSync.stream())
 ));
 
 gulp.task("images", () => (
-  gulp.src("./src/img/**/*")
-    .pipe(gulp.dest(DEST+"img"))
+    gulp.src("./src/img/**/*")
+    .pipe(gulp.dest(DEST + "img"))
     .pipe(browserSync.stream())
 ));
 
-gulp.task("server", ["hugo", "css", "js", "fonts", "images"], () => {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    },
-    notify: false
-  });
-  gulp.watch("./src/js/**/*.js", ["js"]);
-  gulp.watch("./src/css/**/*.css", ["css"]);
-  gulp.watch("./src/img/**/*", ["images"]);
-  gulp.watch("./src/fonts/**/*", ["fonts"]);
-  gulp.watch("./site/**/*", ["hugo"]);
+gulp.task("hugo", (cb) => buildSite(cb));
+gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
+
+gulp.task("build", gulp.series("hugo", "css", "js", "fonts", "images"));
+gulp.task("build-preview", gulp.series("hugo-preview", "css", "js", "fonts", "images"));
+
+gulp.task("server", gulp.series("hugo", "css", "js", "fonts", "images"), () => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        notify: false
+    });
+    gulp.watch("./src/js/**/*.js", ["js"]);
+    gulp.watch("./src/css/**/*.css", ["css"]);
+    gulp.watch("./src/img/**/*", ["images"]);
+    gulp.watch("./src/fonts/**/*", ["fonts"]);
+    gulp.watch("./site/**/*", ["hugo"]);
 });
 
 function buildSite(cb, options) {
-  const args = options ? defaultArgs.concat(options) : defaultArgs;
+    const args = options ? defaultArgs.concat(options) : defaultArgs;
 
-  return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
-    if (code === 0) {
-      browserSync.reload();
-      cb();
-    } else {
-      browserSync.notify("Hugo build failed :(");
-      cb("Hugo build failed");
-    }
-  });
+    return cp.spawn(hugoBin, args, {
+        stdio: "inherit"
+    }).on("close", (code) => {
+        if (code === 0) {
+            browserSync.reload();
+            cb();
+        } else {
+            browserSync.notify("Hugo build failed :(");
+            cb("Hugo build failed");
+        }
+    });
 }
