05858eb9a86fc4dcda292ab672ac7a960b770baa
Peter Strömberg <pez@pezius.com>
2018-11-09 13:48:49 +0100

    Use classList.toggle for toggling `scrolled` class on `header` element
    NB: Won't work in IE9.

--- src/js/app.js
+++ src/js/app.js
@@ -11,39 +11,12 @@ $(function(){
 
 // Sticky Nav Functionality in Vanilla JS
 
-function hasClass(el, className) {
-  if (el.classList)
-    return el.classList.contains(className)
-  else
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
-}
-
-function addClass(el, className) {
-  if (el.classList)
-    el.classList.add(className)
-  else if (!hasClass(el, className)) el.className += " " + className
-}
-
-function removeClass(el, className) {
-  if (el.classList)
-    el.classList.remove(className)
-  else if (hasClass(el, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    el.className=el.className.replace(reg, ' ')
-  }
-}
-
 var header = document.getElementById("header");
 
 window.onscroll = function() {
-
   var currentWindowPos = document.documentElement.scrollTop || document.body.scrollTop;
 
-  if (currentWindowPos > 0) {
-    addClass(header, 'scrolled');
-  } else {
-    removeClass(header, 'scrolled');
-  }
+  header.classList.toggle('scrolled', currentWindowPos > 0);
 };
 
 
