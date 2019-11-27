c0dbff1e1ea4ab397f6fd9ee88be312e273e8eb6
Phil Hawksworth <phil@netlify.com>
2019-11-27 17:56:55 +0000

    Scaffold tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,52 +1,45 @@
+const { DateTime }  = require('luxon');
+const util          = require('util');
 
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
-
-module.exports = function(config) {
-
-  // A useful way to reference the context we are runing eleventy in
-  let env = process.env.ELEVENTY_ENV;
-
-  // Layout aliases can make templates more portable
-  config.addLayoutAlias('default', 'layouts/base.njk');
-
-  // Add some utility filters
-  config.addFilter("squash", require("./src/utils/filters/squash.js") );
-  config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );
+module.exports = function(eleventyConfig) {
 
 
-  // add support for syntax highlighting
-  config.addPlugin(syntaxHighlight);
+  // eleventyConfig.addCollection("posts", function(collection) {
+  //   return collection.getFilteredByGlob("src/site/blog/*.md").reverse();
+  // });
 
-  // minify the html output
-  config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
+  // Layout aliases for convenience
+  eleventyConfig.addLayoutAlias('default', 'layouts/base.njk');
 
-  // compress and combine js files
-  config.addFilter("jsmin", function(code) {
-    const UglifyJS = require("uglify-js");
-    let minified = UglifyJS.minify(code);
-      if( minified.error ) {
-          console.log("UglifyJS error: ", minified.error);
-          return code;
-      }
-      return minified.code;
+  // a debug utility
+  eleventyConfig.addFilter('dump', obj => {
+    return util.inspect(obj)
   });
 
+  // Date helpers
+  eleventyConfig.addFilter('readableDate', dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('LLLL d, y');
+  });
+  eleventyConfig.addFilter('htmlDate', dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('y-MM-dd');
+  });
 
-  // pass some assets right through
-  config.addPassthroughCopy("./src/site/images");
+  // Grab experpts and sections from a file
+  eleventyConfig.addFilter("section", require("./src/filters/section.js") );
 
-  // make the seed target act like prod
-  env = (env=="seed") ? "prod" : env;
-  return {
+  // Static assets to pass through
+  eleventyConfig.addPassthroughCopy("./src/site/fonts");
+
+  return  {
     dir: {
       input: "src/site",
-      output: "dist",
-      data: `_data/${env}`
+      inludes: "_includes",
+      output: "dist"
     },
-    templateFormats : ["njk", "md", "11ty.js"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
     passthroughFileCopy: true
   };
 
