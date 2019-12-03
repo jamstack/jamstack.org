280f37175a3f67de405febe00bb5039ff4a77c82
Phil Hawksworth <phil@netlify.com>
2019-12-03 11:04:07 +0000

    node env switcheroo

--- postcss.config.js
+++ postcss.config.js
@@ -9,14 +9,11 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 })
 
-
-const cssnano = require('cssnano');
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []
+    ...process.env.NODE_ENV !== 'local' ? [purgecss,  require('cssnano')] : []
 
   ]
 };
