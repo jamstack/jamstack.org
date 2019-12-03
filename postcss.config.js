c637e2c13b88f12a77dc14e4b94256cc7430c3f4
Phil Hawksworth <phil@netlify.com>
2019-12-03 10:52:46 +0000

    Remove explict prod env

--- postcss.config.js
+++ postcss.config.js
@@ -10,11 +10,13 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 })
 
 
+const cssnano = require('cssnano');
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production' ? [purgecss, require('cssnano')] : []
+    ...process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []
 
   ]
 };
