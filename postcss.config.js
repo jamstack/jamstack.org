75e9b6a08f13fed7935c19d7de3f68404796a23b
Phil Hawksworth <phil@netlify.com>
2020-04-03 19:02:31 +0100

    Simplified community

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
 
-  // Specify the paths to all of the template files in your project
+  // Specify the paths to all of the template files in the project
   content: [
     './src/site/**/*.njk'
   ],
 
-  // Include any special characters you're using in this regular expression
+  // Include any special characters
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 })
 
