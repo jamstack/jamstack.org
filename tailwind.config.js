126b0b89b504124f90a4f5226b2ac172e7485aca
Zach Leatherman <zachleatherman@gmail.com>
2020-09-25 18:03:29 -0500

    Current status.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,28 @@ module.exports = {
     extend: {
       colors: {
         blue : {
-          900: '#0E182A',
-          800: '#252D3B',
-          300: '#718096',
-          200: '#CBD5E0',
-          100: '#E2E8F0'
+          900: '#0E182A', // old
+          800: '#252D3B', // old
+          300: '#718096', // old
+          200: '#CBD5E0', // old
+          100: '#E2E8F0' // old
+        },
+        gray: {
+          900: "#0D0F18",
+          700: "#2D3247",
+          400: "#5A5F75",
+          200: "#DEDEDE"
         },
         pink : {
-          900 : '#D81467',
-          500 : '#FF54AC',
-          100 : '#FFB1C5'
+          900 : '#D81467', // old
+          500 : '#F0047F',
+          100 : '#FFB1C5' // old
         }
       }
     }
   },
-  variants: {},
+  variants: {
+    fill: ["hover"]
+  },
   plugins: []
 }
