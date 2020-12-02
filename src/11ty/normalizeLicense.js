8bfe4383de25d6810f9d01a1469a4b5904434ec2
Zach Leatherman <zachleatherman@gmail.com>
2020-12-02 10:44:29 -0600

    A tweak to the warning copy

--- src/11ty/normalizeLicense.js
+++ src/11ty/normalizeLicense.js
@@ -5,6 +5,6 @@ module.exports = function(license, title) {
   if(correctedLicenseName) {
     return correctedLicenseName;
   }
-  console.log( `WARN Invalid license name for ${title}:`, license );
+  console.log( `WARN License name (${license}) for ${title} not found on the approved SPDX License List: https://spdx.org/licenses/` );
   return license;
 };
