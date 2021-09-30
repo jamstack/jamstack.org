455b7bfbdb7fa0603b503d472c931bcae097d139
Zach Leatherman <zachleatherman@gmail.com>
2021-09-30 10:04:05 -0500

    Show Chart Data <details>

--- src/js/d3chart-survey.js
+++ src/js/d3chart-survey.js
@@ -1,4 +1,4 @@
-new D3VerticalBarChart("yoe-2020-vert", "yoe-2020-table", {
+new D3VerticalBarChart("yoe-2020-vert", "yoe-2020-vert-table", {
   showInlineBarValues: "outside",
 });
 
@@ -8,20 +8,20 @@ new D3HorizontalBarChart("yoe-2020", "yoe-2020-table", {
   }
 });
 
-new D3VerticalBarChart("yoe-2021-vert", "yoe-2021-table", {
+new D3VerticalBarChart("yoe-2021-vert", "yoe-2021-vert-table", {
 });
 new D3HorizontalBarChart("yoe-2021", "yoe-2021-table", {
 });
 
-new D3VerticalBarChart("yoe-stack-vert", "yoe-stack-table");
+new D3VerticalBarChart("yoe-stack-vert", "yoe-stack-vert-table");
 
 new D3HorizontalBarChart("yoe-stack", "yoe-stack-table");
 
-new D3HorizontalBarChart("yoe-usercount-stacked", "yoe-usercount-table", {
+new D3HorizontalBarChart("yoe-usercount-stacked", "yoe-usercount-stacked-table", {
   mode: "stacked",
 });
 
-new D3VerticalBarChart("yoe-usercount-vert-stacked", "yoe-usercount-table", {
+new D3VerticalBarChart("yoe-usercount-vert-stacked", "yoe-usercount-vert-stacked-table", {
   mode: "stacked",
 });
 
