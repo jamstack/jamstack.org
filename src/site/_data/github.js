a11f43e51a4299b5db1e0e69135adbc0ee8aac0c
hexagoncircle <ryan.mulligan@netlify.com>
2023-07-28 12:39:58 -0700

    Remove mentions and links to retired Jamstack discord

--- src/site/_data/github.js
+++ src/site/_data/github.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const CacheAsset = require("@11ty/eleventy-cache-assets");
 const fastglob = require("fast-glob");
@@ -30,7 +30,7 @@ async function githubRequest(user, repo) {
     issues: "",
   };
 
-  if(process.env.ELEVENTY_ENV == 'dev' || !process.env.GITHUB_READ_TOKEN) {
+  if (!process.env.GITHUB_READ_TOKEN) {
     return errorData;
   }
 
@@ -55,22 +55,22 @@ async function githubRequest(user, repo) {
   const fetchOptions = {
     method: "POST",
     headers: {
-      "Authorization": `bearer ${process.env.GITHUB_READ_TOKEN}`
+      Authorization: `bearer ${process.env.GITHUB_READ_TOKEN}`,
     },
-    body: JSON.stringify({ query })
+    body: JSON.stringify({ query }),
   };
   const opts = {
     duration: "1401m", // 23.5 hours
     type: "json",
-    fetchOptions
+    fetchOptions,
   };
 
   let req;
   try {
     req = await CacheAsset(url, opts);
-    if(req.errors && req.errors.length) {
-      console.log( "GitHub Data Source Error from API", req.errors );
-      if(req.errors.filter(e => e.type === "RATE_LIMITED").length > 0) {
+    if (req.errors && req.errors.length) {
+      console.log("GitHub Data Source Error from API", req.errors);
+      if (req.errors.filter((e) => e.type === "RATE_LIMITED").length > 0) {
         throw new Error("Failing the build due to GitHub API rate limiting.");
       }
       return errorData;
@@ -80,9 +80,9 @@ async function githubRequest(user, repo) {
       stars: req.data.repository.stargazers.totalCount,
       forks: req.data.repository.forks.totalCount,
       issues: req.data.repository.issues.totalCount,
-    }
-  } catch(e) {
-    console.log( "GitHub Data Source Error", e );
+    };
+  } catch (e) {
+    console.log("GitHub Data Source Error", e);
 
     return errorData;
   }
@@ -90,21 +90,21 @@ async function githubRequest(user, repo) {
 
 async function getReposFromMarkdown(glob) {
   // Starters
-	let ssgs = await fastglob(glob, {
-		caseSensitiveMatch: false
+  let ssgs = await fastglob(glob, {
+    caseSensitiveMatch: false,
   });
 
   let repos = [];
-	for(let ssg of ssgs) {
+  for (let ssg of ssgs) {
     let matter = graymatter.read(ssg);
     let fullRepo = matter.data.repo;
-    if(fullRepo) {
+    if (fullRepo) {
       let split = fullRepo.split("/");
       let user = split[0];
       let repo = split[1];
-      
-      if(!matter.data.repohost || matter.data.repohost === "github") {
-        if(matter.data.disabled) {
+
+      if (!matter.data.repohost || matter.data.repohost === "github") {
+        if (matter.data.disabled) {
           continue;
         }
         repos.push({ user, repo });
@@ -115,17 +115,23 @@ async function getReposFromMarkdown(glob) {
   return repos;
 }
 
-module.exports = async function() {
+module.exports = async function () {
   let data = {};
 
   let ssgRepos = await getReposFromMarkdown("./src/site/generators/*.md");
-  for(let entry of ssgRepos) {
-    data[`${entry.user}/${entry.repo}`] = await githubRequest(entry.user, entry.repo);
+  for (let entry of ssgRepos) {
+    data[`${entry.user}/${entry.repo}`] = await githubRequest(
+      entry.user,
+      entry.repo
+    );
   }
 
   let cmsRepos = await getReposFromMarkdown("./src/site/headless-cms/*.md");
-  for(let entry of cmsRepos) {
-    data[`${entry.user}/${entry.repo}`] = await githubRequest(entry.user, entry.repo);
+  for (let entry of cmsRepos) {
+    data[`${entry.user}/${entry.repo}`] = await githubRequest(
+      entry.user,
+      entry.repo
+    );
   }
 
   return data;
